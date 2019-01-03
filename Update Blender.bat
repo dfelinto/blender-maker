4b3be5eb32f261986d811cbd9b27ee29bf463b27
Dalai Felinto <dfelinto@gmail.com>
2019-01-03 14:07:46 -0200

    Update Update Blender.bat
    blender2.8 is not master

--- Update Blender.bat
+++ Update Blender.bat
@@ -25,7 +25,7 @@ git pull
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-cd C:\src\blender\release\blender2.8
+cd C:\src\blender\release\master
 devenv Blender.sln /Build Release /Project INSTALL
 
 pause
